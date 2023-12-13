a9880ab3ba687ae9e319e7c7d60f1bb320ff218a
Matheus Santana <santalvesmatheus@gmail.com>
2023-12-13 15:45:32 -0300

    created homepage and image-cutter

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+
+export const Home = () => {
+    return (
+        <div className="hero min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-md flex flex-col justify-center">
+                    <img src={'https://i.postimg.cc/cJnKqhgV/logo-ismafer.jpg'} alt='logo' width={200}  className="self-center"/>
+                    <p className="py-6 font-light text-[--light]">Ferramentas de uso rápido para criação de anúncios da Ismafer.</p>
+                    <div className="flex flex-col gap-4">
+                        <Link href={'/tools/cutter'}>
+                        <button className="btn bg-[--brand]  hover:bg-orange-500 text-[--dark] text-xl w-full">Cortador de imagens</button>
+                        </Link>
+                        <Link href={'/bg-remover'}>
+                        <button className="btn bg-[--brand]  hover:bg-orange-500 text-[--dark] text-xl w-full">Remover fundo</button>
+                        </Link>
+                        <Link href={'https://kitcreator.vercel.app/'}>
+                        <button className="btn bg-[--brand]  hover:bg-orange-500 text-[--dark] text-xl w-full">Criação de KIT</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
